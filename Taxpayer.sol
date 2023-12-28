4d1a157d5a54aaba0254e0f0447d36947a45e28b
Owanesh <24321561+Owanesh@users.noreply.github.com>
2023-12-28 13:37:26 +0100

    style: adds `view` on getters() and SPDX License Identifier

--- Taxpayer.sol
+++ Taxpayer.sol
@@ -1,75 +1,75 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
 contract Taxpayer {
+    uint age;
 
- uint age; 
+    bool isMarried;
 
- bool isMarried; 
+    /* Reference to spouse if person is married, address(0) otherwise */
+    address spouse;
 
- /* Reference to spouse if person is married, address(0) otherwise */
- address spouse; 
+    address partner1;
+    address partner2;
 
+    /* Constant default income tax allowance */
+    uint constant DEFAULT_ALLOWANCE = 5000;
 
-address  parent1; 
-address  parent2; 
+    /* Constant income tax allowance for Older Taxpayers over 65 */
+    uint constant ALLOWANCE_OAP = 7000;
 
- /* Constant default income tax allowance */
- uint constant  DEFAULT_ALLOWANCE = 5000;
+    /* Income tax allowance */
+    uint tax_allowance;
 
- /* Constant income tax allowance for Older Taxpayers over 65 */
-  uint constant ALLOWANCE_OAP = 7000;
+    uint income;
 
- /* Income tax allowance */
- uint tax_allowance; 
+    constructor(address p1, address p2) {
+        age = 0;
+        isMarried = false;
+        partner1 = p1;
+        partner2 = p2;
+        spouse = address(0);
+        income = 0;
+        tax_allowance = DEFAULT_ALLOWANCE;
+    }
 
- uint income; 
+    //We require new_spouse != address(0);
+    function marry(address new_spouse) public {
+        spouse = new_spouse;
+        isMarried = true;
+    }
 
+    function divorce() public {
+        Taxpayer sp = Taxpayer(address(spouse));
+        sp.setSpouse(address(0));
+        spouse = address(0);
+        isMarried = false;
+    }
 
- constructor(address p1, address p2) {
-   age = 0;
-   isMarried = false;
-   parent1 = p1;
-   parent2 = p2;
-   spouse = address(0);
-   income = 0;
-   tax_allowance = DEFAULT_ALLOWANCE;
- } 
+    /* Transfer part of tax allowance to own spouse */
+    function transferAllowance(uint change) public {
+        tax_allowance = tax_allowance - change;
+        Taxpayer sp = Taxpayer(address(spouse));
+        sp.setTaxAllowance(sp.getTaxAllowance() + change);
+    }
 
+    function haveBirthday() public {
+        age++;
+    }
 
- //We require new_spouse != address(0);
- function marry(address new_spouse) public {
-  spouse = new_spouse;
-  isMarried = true;
- }
- 
- function divorce() public {
-  Taxpayer sp = Taxpayer(address(spouse));
-  sp.setSpouse(address(0));
-  spouse = address(0);
-  isMarried = false;
- }
+    function setSpouse(address sp) public {
+        spouse = sp;
+    }
 
- /* Transfer part of tax allowance to own spouse */
- function transferAllowance(uint change) public {
-  tax_allowance = tax_allowance - change;
-  Taxpayer sp = Taxpayer(address(spouse));
-  sp.setTaxAllowance(sp.getTaxAllowance()+change);
- }
+    function getSpouse() public view returns (address) {
+        return spouse;
+    }
 
- function haveBirthday() public {
-  age++;
- }
+    function setTaxAllowance(uint ta) public {
+        tax_allowance = ta;
+    }
 
-function setSpouse(address sp) public {
-    spouse = sp;
-}
-function getSpouse() public returns (address) {
-    return spouse;
-}
-function setTaxAllowance(uint ta) public {
-    tax_allowance = ta;
-}
-function getTaxAllowance() public returns (uint) {
-    return tax_allowance;
-}
+    function getTaxAllowance() public view returns (uint) {
+        return tax_allowance;
+    }
 }
