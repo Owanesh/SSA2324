0d874dff9894a90cf1c7a25c61d87705814b4728
Owanesh <24321561+Owanesh@users.noreply.github.com>
2023-12-29 16:40:06 +0100

    tests: adds divorce() testing

--- TaxpayerTesting.sol
+++ TaxpayerTesting.sol
@@ -3,27 +3,39 @@ pragma solidity ^0.8.22;
 
 import "./Taxpayer.sol"; // Replace with the actual path to your contract
 
-contract TaxpayerTesting  is Taxpayer{
+contract TaxpayerTesting is Taxpayer {
     Taxpayer bravo;
+
     constructor() Taxpayer(address(0), address(0)) {}
 
     function echidna_test_one_way_marriage() public returns (bool) {
         bravo = new Taxpayer(address(0), address(0));
         this.marry(address(bravo));
-        bool this_to_bravo =  this.getSpouse()==address(bravo)&& this.getSpouse()!=address(0);
+        bool this_to_bravo = this.getSpouse() == address(bravo) &&
+            this.getSpouse() != address(0);
         return this_to_bravo;
     }
 
-
     function echidna_test_both_married() public returns (bool) {
-        bravo = new Taxpayer(address(1), address(1));
+        bravo = new Taxpayer(address(0), address(0));
         this.marry(address(bravo));
         bravo.marry(address(this));
-        bool this_to_bravo =  this.getSpouse()==address(bravo)&& this.getSpouse()!=address(0);
-        bool bravo_to_this =  bravo.getSpouse()==address(this)&& bravo.getSpouse()!=address(0);
-        return this_to_bravo&&bravo_to_this;
+        bool this_to_bravo = this.getSpouse() == address(bravo) &&
+            this.getSpouse() != address(0);
+        bool bravo_to_this = bravo.getSpouse() == address(this) &&
+            bravo.getSpouse() != address(0);
+        return this_to_bravo && bravo_to_this;
     }
 
+    function echidna_test_divorce() public returns (bool) {
+        bravo = new Taxpayer(address(0), address(0));
+        this.marry(address(bravo));
+        bravo.marry(address(this));
+        bravo.divorce();
+        bool this_divorced = this.getSpouse() == address(0);
+        bool bravo_divorced = bravo.getSpouse() == address(0);
+        return this_divorced && bravo_divorced;
+    }
 
     // Echidna test to check age increment
     function echidna_test_have_birthday() public returns (bool) {
