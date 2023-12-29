8300e030648e54a7818c1cbfd09b5173604c4f30
Owanesh <24321561+Owanesh@users.noreply.github.com>
2023-12-29 22:53:59 +0100

    feat(ci): adds echidna on github actions

--- TaxpayerTesting.sol
+++ TaxpayerTesting.sol
@@ -5,7 +5,7 @@ import "./Taxpayer.sol"; // Replace with the actual path to your contract
 
 contract TaxpayerTesting is Taxpayer {
     constructor() Taxpayer(address(0), address(0)) {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 18; i++) {
             haveBirthday();
         }
     }
